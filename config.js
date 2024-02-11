4e9fd772a51c62b22194fa221a7472f7fb171d31
dinkafilms <159150600+dinkafilms@users.noreply.github.com>
2024-02-11 13:57:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94716769285', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94728840491', 'me', true],
+  ['94728840491', 'Nadeen Poorna', true], 
   [''] 
 ] //Number of owners
 
